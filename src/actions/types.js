c9805afc00ee8e7e20dd557a14ea482ee23190e8
Kar Sheng Lee <karshenglee@Kars-MacBook-Air.local>
2017-06-05 19:41:22 +0800

    left out this one on previous commit

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,6 +2,4 @@ export const AUTH_CLIENT = 'auth_client';
 export const UNAUTH_CLIENT = 'unauth_client';
 export const AUTH_ERROR = 'auth_error';
 
-export const CLIENT_TOKEN_NAME = 'peach-token';
-
 export const FETCH_RECOMMENDATIONS = 'fetch-recommendations';
